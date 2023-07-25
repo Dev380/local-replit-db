f9223ebbc94a9f07b75b56f8ef38dfc9b4947f7c
Dev381 <49997896+Dev380@users.noreply.github.com>
2023-07-25 16:09:23 -0400

    I swear if THIS doesn't work

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
 """Replit DB-style API with a local file backing it"""
 __name__ = "localreplitdb"
+from .replitdb import Database as ReplitDatabase
+from .localreplitdb import LocalDatabase, db, db_path
